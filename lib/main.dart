15fabaea9ecb9afe5f417711d8392d4210ac2faf
Abigail Lima <abigailmvlima@outlook.com>
2024-08-09 09:51:33 -0300

    fiz o initState, addTodo, loadData e onchange

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+  List _todoList = [];
 
-  final List _todoList = ["Daniel, Davi"];
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  void _addTodo() {
+    setState(() {
+      Map<String, dynamic> newTodo = {};
+      newTodo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newTodo["ok"] = false;
+      _todoList.add(newTodo);
+      _saveData();  // Salva os dados após adicionar uma nova tarefa
+    });
+  }
+
+  void _loadData() async {
+    String? data = await _readData();
+    if (data != null) {
+      setState(() {
+        _todoList = json.decode(data);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,24 +62,24 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
             child: Row(
               children: [
-                const Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: "Nova tarefa",
-                        labelStyle: TextStyle(color: Colors.orangeAccent),
-                      ),
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: const InputDecoration(
+                      labelText: "Nova tarefa",
+                      labelStyle: TextStyle(color: Colors.orangeAccent),
                     ),
+                  ),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: _addTodo,
                   style: ElevatedButton.styleFrom(
-                         backgroundColor: Colors.orangeAccent,
-                         shape: RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(4),
-                         ) ,
-                    textStyle: const TextStyle(color: Colors.white)
-                  ),
-                  child: const Text("ADD")
+                      backgroundColor: Colors.orangeAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      textStyle: const TextStyle(color: Colors.white)),
+                  child: const Text("ADD"),
                 )
               ],
             ),
@@ -61,17 +87,26 @@ class _HomeState extends State<Home> {
           Expanded(
             child: ListView.builder(
               padding: const EdgeInsets.only(top: 10.0),
-                itemCount: _todoList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_todoList[index]),
-                  );
-                }),
+              itemCount: _todoList.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  title: Text(_todoList[index]["title"]),
+                  value: _todoList[index]["ok"],
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _todoList[index]["ok"] = value;
+                      _saveData();  // Salva os dados após alterar o estado de uma tarefa
+                    });
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
     );
   }
+
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
@@ -86,12 +121,9 @@ class _HomeState extends State<Home> {
   Future<String?> _readData() async {
     try {
       final file = await _getFile();
-
       return file.readAsString();
-    }catch (e) {
+    } catch (e) {
       return null;
     }
   }
 }
-
-
